a75f329b77fc3799e440683dd13d4cd5fd62dc15
Can Baran Gündüz <canbaran011@gmail.com>
2022-09-19 07:41:32 +0300

    dyn-comps

---
+++ 13-vue-dyn-comp/dyn-comps/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button @click="component = 'form-one'">show form one</button>
+    <button @click="component = 'form-two'">show form two</button>
+  </div>
+</template>
+
+<script>
+
+import formOne from './components/formOne.vue'
+import formTwo from './components/formTwo.vue'
+
+export default {
+  components:{
+    'form-one':formOne,
+    'form-two':formTwo
+  },
+  data () {
+    return {
+      component:'form-one'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
